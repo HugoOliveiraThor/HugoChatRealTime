56589ffa31dbd3394b35ec4bc7a1a4ee457de48f
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2016-02-21 20:53:14 -0300

    Todo projeto

--- app.js
+++ app.js
@@ -1,61 +1,40 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var usuarios = {};
+server.listen(3000);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/',function (req,res){
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-// error handlers
+io.sockets.on('connection',function(socket){
+    socket.on('novo usuario',function(nickname,callback){
+        var retorno = {};
+        if(nickname in usuarios){
+        callback({retorno:false , msg:"O usuário já está em uso"});
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+        }else{
+            console.log('Novo usuário no chat'+nickname);
+            callback({retorno:true , msg: ""});
+            socket.nickname = nickname;
+            usuarios[socket.nickname] = socket;
+            atualizarUsuarios();
+
+        }
     });
-  });
-}
+    socket.on('enviar mensagem',function(data){
+        var mensagem  = data.trim();
+        io.sockets.emit('nova mensagem',{msg:mensagem , nick: socket.nickname});
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+    });
+    socket.on('disconnect',function(){
+        if(!socket.nickname) return;
+        delete usuarios[socket.nickname];
+    });
+    function atualizarUsuarios(){
+        io.sockets.emit('atualiza usuarios', Object.keys(usuarios));
 
-
-module.exports = app;
+    }
+})
